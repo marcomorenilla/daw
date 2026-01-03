15f842b28bf5018b9364989e8358222926302431
marco <marcomorenilla@gmail.com>
2026-01-03 13:01:52 +0100

    termino ejercicio

--- dec/ejercicio-dom/js/formulario.js
+++ dec/ejercicio-dom/js/formulario.js
@@ -1,15 +1,75 @@
-const form = document.getElementById("contact");
-
-
-let data = [];
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('doc cargato')
+
+    console.log('doc cargado');
+    const form = document.getElementById("contact");
+    const table = document.getElementById("table-div");
+    const warn = document.getElementById("warn");
+    const tbody = document.getElementById("t-body");
+    const btnEliminar = document.getElementById('eliminar')
+
+    let tableData = JSON.parse(localStorage.getItem('data')) || [];
+
+    function showTable() {
+        if (tableData.length > 0) {
+            warn.style = "display:none;";
+            table.style = "display:block";
+        } else {
+            warn.style = "display:inline-block;";
+            table.style = "display:none";
+        }
+    }
+
+    function generateTableRow(data = {}) {
+        console.log(`${JSON.stringify(data, null, 4)}`)
+        const fila = document.createElement('tr');
+        const celdaNombre = `<td>${data.nombreData}</td>`
+        const celdaEmail = `<td>${data.emailData}</td>`
+        const celdamensaje = `<td>${data.mensajeData}</td>`
+
+        fila.innerHTML = `${celdaNombre}${celdaEmail}${celdamensaje}`
+        tbody.appendChild(fila)
+    }
+
+    function saveData(data = {}) {
+        tableData.push(data);
+        generateTableRow(data);
+        showTable();
+        localStorage.setItem('data', JSON.stringify(tableData));
+    }
+
+    function removeData() {
+        localStorage.clear();
+        tableData = []
+        tbody.innerHTML = '';
+        showTable();
+    }
+
+    tableData.forEach(d => {
+        generateTableRow(d)
+    })
+
+    showTable();
+
+    btnEliminar.addEventListener('click', removeData);
+
     form.addEventListener('submit', (e) => {
         e.preventDefault();
         console.log('form añadido')
-        let nombreData = document.getElementById("nombre").value
-        let emailData = document.getElementById("email").value
-        let mensajeData = document.getElementById("mensaje").value
-        console.log(`Datos introducidos: ${nombreData}, ${emailData}, ${mensajeData}`)
+
+
+        const nombreData = document.getElementById("nombre").value
+        const emailData = document.getElementById("email").value
+        const mensajeData = document.getElementById("mensaje").value
+        if (nombreData && emailData && mensajeData) {
+            const res = confirm(`Deseas añadir los siguientes datos?
+                nombre: ${nombreData}
+                email: ${emailData}
+                mensaje: ${mensajeData}`
+            )
+            res ? saveData({ nombreData, emailData, mensajeData }) : alert('Operación canelada')
+
+        } else {
+            alert('faltan datos por rellenar')
+        }
     })
 })
